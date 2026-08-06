1eb14a0dd9cb6086db8610c6daa8ca42a1ed9584
sebachacon <ch18082008@gmail.com>
2026-08-06 13:52:00 -0600

    Crear proyecto, componenetes principales e hijos

---
+++ src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+{
+  path: 'dashboard',
+  loadComponent: () =>
+    import('./gifs/pages/dashboard-page/dashboard-page'),
+},
+{
+  path: 'trending',
+  loadComponent: () =>
+    import('./gifs/pages/trending-page/trending-page'),
+},
+{
+  path: 'search',
+  loadComponent: () =>
+    import('./gifs/pages/search-page/search-page'),
+},
+
+{
+  path: '**',
+  redirectTo: 'dashboard',
+},
+
+
+];
