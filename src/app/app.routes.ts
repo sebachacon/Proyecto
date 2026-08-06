b0d658cdfaf4dabde0afed6736e8a10292c2d0cf
sebachacon <ch18082008@gmail.com>
2026-08-06 15:53:31 -0600

    Rutas hijas incompleta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,8 @@ export const routes: Routes = [
   path: 'dashboard',
   loadComponent: () =>
     import('./gifs/pages/dashboard-page/dashboard-page'),
-},
-{
+  children: [
+    {
   path: 'trending',
   loadComponent: () =>
     import('./gifs/pages/trending-page/trending-page'),
@@ -16,6 +16,8 @@ export const routes: Routes = [
   path: 'search',
   loadComponent: () =>
     import('./gifs/pages/search-page/search-page'),
+},
+  ],
 },
 
 {
