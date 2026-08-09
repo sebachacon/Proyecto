6beace143ffb65b72020c000080e1f19d755039d
sebachacon <ch18082008@gmail.com>
2026-08-09 16:19:01 -0600

    Agregar RouterLinks desde componentes hijo

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions {}
+export class SideMenuOptions {
+  menuOptions:MenuOption[] = [
+    {
+       icon: 'fa-solid fa-chart-line',
+       label: 'Trending',
+       subLabel: 'Gifs Populares',
+       route: '/dashboard/trending'
+    },
+     {
+       icon: 'fa-solid fa-magnifying-glass',
+       label: 'Buscador',
+       subLabel: 'Buscar gifs',
+       route: '/dashboard/search'
+    },
+  ];
+}
