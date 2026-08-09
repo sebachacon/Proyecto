10a173c07d325500597363c1e7857390036fd540
sebachacon <ch18082008@gmail.com>
2026-08-09 15:13:56 -0600

    Crear componentes para el menu lateral

--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenu } from '../../components/side-menu/side-menu';
 
 @Component({
   selector: 'app-dashboard-page',
   imports: [
-    RouterOutlet
-  ],
+    RouterOutlet, SideMenu],
   templateUrl: './dashboard-page.html',
 })
 export default class DashboardPage {}
